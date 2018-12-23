318cd250bb8b5d12071529d438ae4cb378d1bbee
summertech <summertech@10.144.4.31>
2018-12-23 11:52:37 -0500

    turtle

--- NR/thefile.py
+++ NR/thefile.py
@@ -1,1 +1,8 @@
-print("test")
+import turtle
+scr=turtle.Screen()
+t=turtle.Turtle()
+for i in range(4):
+    t.forward(100)
+    t.right(90)
+scr.exitonclick()
+#https://docs.python.org/3.3/library/turtle.html?highlight=turtle#turtle.forward
