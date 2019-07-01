fdb65a7d1deeba8a392a65812aae3f7459fbf558
rcnrcn927 <steve@steve.com>
2019-07-01 09:31:40 -0400

    second test

--- NR/thefile.py
+++ NR/thefile.py
@@ -1,8 +1,8 @@
 import turtle
 scr=turtle.Screen()
 t=turtle.Turtle()
-for i in range(4):
-    t.forward(100)
-    t.right(90)
+for i in range(8):
+    t.forward(70)
+    t.right(45)
 scr.exitonclick()
 #https://docs.python.org/3.3/library/turtle.html?highlight=turtle#turtle.forward
