2a16e0bfc394e60c137a330ed391f0c0d7dbde38
rcnrcn927 <steve@steve.com>
2019-07-01 11:13:20 -0400

    fixed lol

--- NR/vstestbubblesort.py
+++ NR/vstestbubblesort.py
@@ -7,7 +7,7 @@ print(l)
 for a in range(len(l)):
     for b in range(len(l)):
         if l[a]<l[b]:
-            temp=l[a]20
+            temp=l[a]
             l[a]=l[b]
             l[b]=temp
 print(l)
