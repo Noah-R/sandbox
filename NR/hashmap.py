790f6d4e8755292b0f5910c25018090d81b0cfa4
SummerTech <stusr@summertech.net>
2019-07-12 15:38:44 -0400

    hashmap py

---
+++ NR/hashmap.py
@@ -0,0 +1,13 @@
+class Node():
+    def __init__(self, data):
+        self.data=data
+    def getData(self):
+        return data
+    def setData(self, data):
+        self.data=data
+        return None
+a=Node("aeroidarnseochiaerocdiaoerciaoei")
+b=Node("aeroidarnseochiaerocdiaoerciaoei")
+print("this is the first line")
+print(hash(a))
+print(hash(b))
