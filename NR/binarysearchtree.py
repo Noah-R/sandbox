fee65b769f3944f07773b537a1f2aa0f14c66be5
SummerTech <stusr@summertech.net>
2019-07-15 10:00:43 -0400

    f1

--- NR/binarysearchtree.py
+++ NR/binarysearchtree.py
@@ -1,47 +1,72 @@
 class BinarySearchTree():
     def __init__(self):
-        self.top=None
-        self.size=0
+        self.top = None
+        self.size = 0
+
     def addElement(self, element):
-        if(self.top==None):
-            self.top=element
-            self.size=1
+        if(self.top == None):
+            self.top = element
+            self.size = 1
             return True
-        current=self.top
+        current = self.top
         while(True):
-            if(element<current.getData()):
-                next=current.getLeft()
-                if(next==None):
+            if(element < current.getData()):
+                next = current.getLeft()
+                if(next == None):
                     current.setLeft(element)
-                    self.size+=1
+                    self.size += 1
                     return True
                 else:
-                    current=next
-            elif(element<current.getData()):
-                next=current.getRight()
-                if(next==None):
+                    current = next
+            elif(element < current.getData()):
+                next = current.getRight()
+                if(next == None):
                     current.setRight(element)
-                    self.size+=1
+                    self.size += 1
                     return True
                 else:
-                    current=next
+                    current = next
             else:
                 return False
+
     def removeElement(self, element):
-        current=self.top
-        parent=None
+        current = self.top
+        parent = None
         while(True):
-            if(current==None):
+            if(current == None):
                 return False
-            elif(current.getData()==element):
-                #remove and restructure
+            elif(current.getData() == element):
+                if(current.getLeft() != None):
+                    current = current.getLeft()
+                    while(current.getRight() != None):
+                        current = current.getRight()
+                    current = current.getData()
+                    removeElement(current)
+                    addElement(current)
+                    return True
+                elif(current.getRight() != None):
+                    current = current.getRight()
+                    while(current.getLeft() != None):
+                        current = current.getLeft()
+                    current = current.getData()
+                    removeElement(current)
+                    addElement(current)
+                    return True
+                elif(parent.getData() > element):
+                    parent.setLeft(None)
+                    return True
+                elif(parent.getData() < element):
+                    parent.setRight(None)
+                    return True
+                else:
+                    print("Something is seriously wrong - removeElement")
                 return True
-            elif(current.getData()<element):
-                parent=current
-                current=current.getLeft()
-            elif(current.getData()>element):
-                parent=current
-                current=current.getRight()
+            elif(current.getData() < element):
+                parent = current
+                current = current.getLeft()
+            elif(current.getData() > element):
+                parent = current
+                current = current.getRight()
     """
               4
            2     6
@@ -52,38 +77,50 @@ class BinarySearchTree():
          str
          test
     """
+
     def contains(self, element):
         while(True):
-            current=self.top
-            if(current.getData()==element):
+            current = self.top
+            if(current.getData() == element):
                 return True
-            elif(current==None):
+            elif(current == None):
                 return False
-            elif(current.getData()<element):
-                current=current.getLeft()
-            elif(current.getData()>element):
-                current=current.getRight()
+            elif(current.getData() < element):
+                current = current.getLeft()
+            elif(current.getData() > element):
+                current = current.getRight()
+
     def size(self):
         return self.size
+
     def __str__(self):
         return "Starts at"+str(self.top)
+
+
 class Node():
     def __init__(self, data):
-        self.data=data
-        self.left=None
-        self.right=None
+        self.data = data
+        self.left = None
+        self.right = None
+
     def getData(self):
         return self.data
+
     def setData(self, data):
-        self.data=data
+        self.data = data
         return None
+
     def getLeft(self):
         return self.left
+
     def setLeft(self, data):
-        self.left=data
+        self.left = data
         return None
+
     def getRight(self):
         return self.right
+
     def setRight(self, data):
-        self.right=data
+        self.right = data
         return None
+# can do: balancing function
