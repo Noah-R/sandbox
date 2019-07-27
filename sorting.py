87bb175ed14473a00d652ad84c4d99bb75e1000d
SummerTech <stusr@summertech.net>
2019-07-27 18:02:04 -0400

    broadened for future sort implementations

---
+++ sorting.py
@@ -0,0 +1,15 @@
+def bubblesort(l):
+    for a in range(len(l)):
+        for b in range(len(l)):
+            if l[a]<l[b]:
+                temp=l[a]
+                l[a]=l[b]
+                l[b]=temp
+    return l
+from random import randint
+l=[]
+num=int(input("number of items"))
+for i in range(num):
+    l.append(randint(1, 1000))
+print(l)
+print(bubblesort(l))
